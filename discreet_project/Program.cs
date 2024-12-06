f46a06c525f9b9464c6964f428449ed30c447bb7
Nicolas-Sarmiento <nicolas.sarmiento@uptc.edu.co>
2024-12-06 18:54:34 -0500

    new content

--- discreet_project/Program.cs
+++ discreet_project/Program.cs
@@ -18,6 +18,27 @@ namespace discreet_project
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
 
+            List<double> list = new List<double>() { 0.0588,0.1565,0.1598,0.16,0.3,0.58,0.781};
+            double target = 0.3;
+            
+            Search lnSearch = new Search();
+            Search expSearch = new Search();
+
+            long lnCount = lnSearch.linearSearch(list, target); 
+            long xpCount = expSearch.exponentialSearch(list, target);
+
+            Console.WriteLine(lnCount.ToString());
+            Console.WriteLine(xpCount.ToString());
+
+            Console.WriteLine(lnSearch.stopWatchAll.Elapsed.ToString());
+            Console.WriteLine(expSearch.stopWatchAll.Elapsed.ToString());
+
+
+            Console.WriteLine(lnSearch.stopWatchFirst.Elapsed.ToString());
+            Console.WriteLine(expSearch.stopWatchFirst.Elapsed.ToString());
+
+
+
         }
     }
 }
---
+++ discreet_project/Search.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace discreet_project
+{
+    internal class Search
+    {
+        public Stopwatch stopWatchFirst { get; set; }
+        public Stopwatch stopWatchAll { get; set; }
+
+        public Search()
+        {
+            stopWatchAll = new Stopwatch();
+            stopWatchFirst = new Stopwatch();
+        }
+
+        public double roundValue(double number)
+        {
+            double rounded = 0;
+            number *= 10;
+            number = Math.Floor(number);
+            rounded = number / 10;
+            return rounded;
+        }
+
+        public long linearSearch(List<double> list, double value)
+        {
+            long finds = 0;
+            bool findFirst = false;
+
+            stopWatchFirst.Restart();
+            stopWatchAll.Restart();
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (value == roundValue(list[i]))
+                {
+                    if (!findFirst)
+                    {
+                        stopWatchFirst.Stop();
+                        findFirst = true;
+                    }
+                    finds++;
+                }
+
+            }
+            stopWatchFirst.Stop();
+            stopWatchAll.Stop();
+
+            return finds;
+        }
+
+
+        public long exponentialSearch(List<double> list, double value)
+        {
+            long finds = 0;
+            stopWatchFirst.Restart();
+            stopWatchAll.Restart();
+
+            int n = list.Count;
+            int i = 1;
+            while (i < n && roundValue(list[i]) <= value)
+            {
+                i = i * 2;
+            }
+
+            int left = BinarySearch(list, value, 0, Math.Min(n - 1, i), true);
+            int right = BinarySearch(list, value, 0, Math.Min(n - 1, i), false);
+            if (left == -1) finds = 0;
+            else
+            {
+                finds = (right+1) - left ;
+            }
+
+            stopWatchAll.Stop();
+            stopWatchFirst.Stop();
+            return finds;
+        }
+
+        private int BinarySearch(List<double> array, double target, int low, int high, bool lowerBound)
+        {
+            int result = -1;
+            bool foundFirst = false; 
+            while (low <= high)
+            {
+                int mid = low + (high - low) / 2;
+
+                if (roundValue(array[mid]) == target)
+                {
+                    result = mid;
+                    if (!foundFirst)
+                    {
+                        stopWatchFirst.Stop();
+                        foundFirst = true;
+                    }
+                    if (lowerBound)
+                    {
+                        high = mid - 1;
+                    }
+                    else
+                    {
+                        low = mid + 1;
+                    }
+                }
+                else if (roundValue(array[mid]) < target)
+                {
+                    low = mid + 1;
+                }
+                else
+                {
+                    high = mid - 1;
+                }
+            }
+            return result;
+        }
+    }
+}
